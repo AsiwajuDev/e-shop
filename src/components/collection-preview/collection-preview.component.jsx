57ba2e43bec81db2121e02f0fa70c3c8ef0692ff
asiwajudev <olanrewajuranda@gmail.com>
2021-07-17 23:04:23 +0100

    Completed Shop Preview page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 
-import "./collection-preview.component.scss";
+import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => {
   return (
@@ -9,8 +10,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className='preview'>
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div>{item.name}</div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
