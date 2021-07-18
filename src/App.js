d8c001f10d1491bd621f3c281fc8c26ec35a5499
asiwajudev <olanrewajuranda@gmail.com>
2021-07-18 17:53:45 +0100

    Set existing users logged in and unsubscribe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import { Switch, Route } from "react-router-dom";
-
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase.utils";
+import Authentication from "./pages/auth/auth.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Auth from "./pages/auth/auth.component";
 
-import Header from "./components/header/header.component";
-import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/login' component={Auth} />
-      </Switch>
-    </div>
-  );
+  unsuscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsuscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsuscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/login' component={Authentication} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
